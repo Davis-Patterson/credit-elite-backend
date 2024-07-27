9552c6f3bf077be3dae0b010af2b0af7bed26cfe
Davis-Patterson <Davisfpatterson@gmail.com>
2024-07-27 16:51:55 -0400

    modified 'generateAppointments.js' to not convert times to EST, this should only happen once the appointments are requested

--- generateAppointments.js
+++ generateAppointments.js
@@ -4,59 +4,31 @@ require('dotenv').config();
 
 const mongoURI = process.env.MONGO_URI;
 
-const convertESTtoUTC = (date) => {
-  const utcDate = new Date(date);
-  utcDate.setHours(utcDate.getHours() + 5); // Convert EST to UTC (EST+5)
-  return utcDate;
-};
-
 const generateAppointments = async () => {
   try {
     await mongoose.connect(mongoURI);
     console.log('MongoDB connection established');
 
-    const startTime = 12; // 12 PM EST
-    const endTime = 18; // 6 PM EST
+    const startTimeUTC = 17; // 12 PM EST in UTC (17:00 UTC)
+    const endTimeUTC = 23; // 6 PM EST in UTC (23:00 UTC)
     const slotDuration = 30; // 30 minutes
 
     const today = new Date();
     const daysToGenerate = 10;
-
-    const formatTime = (start, end) => {
-      const startHour = start
-        .toLocaleTimeString('en-US', {
-          hour: 'numeric',
-          minute: '2-digit',
-          hour12: true,
-        })
-        .replace(/ (AM|PM)/, '');
-
-      const endHour = end.toLocaleTimeString('en-US', {
-        hour: 'numeric',
-        minute: '2-digit',
-        hour12: true,
-      });
-
-      return `${startHour} - ${endHour}`;
-    };
 
     for (let day = 0; day < daysToGenerate; day++) {
       const date = new Date(today);
       date.setDate(today.getDate() + day);
       date.setHours(0, 0, 0, 0);
 
-      for (let hour = startTime; hour < endTime; hour++) {
+      for (let hour = startTimeUTC; hour < endTimeUTC; hour++) {
         for (let minute = 0; minute < 60; minute += slotDuration) {
-          const appointmentStartDateEST = new Date(date);
-          appointmentStartDateEST.setHours(hour, minute, 0, 0);
-          const appointmentStartDateUTC = convertESTtoUTC(
-            appointmentStartDateEST
-          );
+          const appointmentStartDateUTC = new Date(date);
+          appointmentStartDateUTC.setHours(hour, minute, 0, 0);
 
-          const appointmentEndDateEST = new Date(
-            appointmentStartDateEST.getTime() + slotDuration * 60000
+          const appointmentEndDateUTC = new Date(
+            appointmentStartDateUTC.getTime() + slotDuration * 60000
           );
-          const appointmentEndDateUTC = convertESTtoUTC(appointmentEndDateEST);
 
           // Check for overlapping appointments
           const overlappingAppointment = await Appointment.findOne({
